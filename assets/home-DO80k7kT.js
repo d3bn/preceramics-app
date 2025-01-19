825d42d462dcb7eb71d620de14d0dab019654e3c
d3bn <d3bn@users.noreply.github.com>
2025-01-19 05:19:20 +0000

    deploy: 67dde21a99851904d6d55c9a10b7febc5b21cd8a

---
+++ assets/home-DO80k7kT.js
@@ -0,0 +1,8 @@
+import{j as i,a as s,c as e,m as t}from"./logo-Me6Dv6Ig.js";import"./index-pb6UZbHI.js";function a(){return i("div",{css:n,children:[s("img",{src:t}),i("ul",{css:r,children:[s("li",{children:"Sculptor."}),s("li",{children:"Ceramist."}),s("li",{children:"Artist."})]})]})}const n=e``,r=e`
+  list-style-type: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1.5em;
+  padding: 0;
+`;export{a as default};
